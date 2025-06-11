2c627d227c035c2452ec921f0d1483e1d97a2c17
S.MOHANAKUMAR <96325451+mk-2871@users.noreply.github.com>
2025-06-11 23:10:17 +0530

    Modified OurToken file

--- src/OurToken.sol
+++ src/OurToken.sol
@@ -2,6 +2,12 @@
 
 pragma solidity ^0.8.18;
 
-contract OurToken {}
+import {ERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+
+contract OurToken is ERC20{
+    constructor(uint256 initialSupply) ERC20("OurToken", "OT") {
+        _mint(msg.sender, initialSupply);
+    }
+}
 
 
