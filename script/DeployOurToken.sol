7ceebc06635abcc4ac530cb3838edaf63877187b
S.MOHANAKUMAR <96325451+mk-2871@users.noreply.github.com>
2025-06-11 23:23:51 +0530

    Added OurTokenTest File

--- script/DeployOurToken.sol
+++ script/DeployOurToken.sol
@@ -2,16 +2,16 @@
 
 pragma solidity ^0.8.18;
 
-import {Script} from "forge-std/Script.sol";
+import {Script} from "lib/forge-std/src/Script.sol";
 import {OurToken} from "../src/OurToken.sol";
 
 contract DeployOurToken is Script {
     uint256 public constant INITIAL_SUPPLY = 1000 ether;
 
-    function run() external {
+    function run() external returns (OurToken) {
         vm.startBroadcast();
-        new OurToken(INITIAL_SUPPLY);
+        OurToken token = new OurToken(INITIAL_SUPPLY);
         vm.stopBroadcast();
+        return token;
     }
 }
-
